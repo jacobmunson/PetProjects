7b6971bfbc579b1ec32c797ce53066447da498e1
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2019-10-25 01:27:02 -0600

    Create percent_change_function.R

---
+++ percent_change_function.R
@@ -0,0 +1,14 @@
+###############################
+### Percent Change Function ###
+# 10/24/2019 ##################
+# I find myself wanting to calculate percent change a lot, so I figured I'd throw a function together for it real quick like. 
+
+percent_change = function(first_value, second_value){
+  pct_change = ((second_value - first_value)/first_value) * 100
+  return(pct_change)
+}
+
+
+percent_change(first_value = 144, second_value = 1295) # +799.3% change
+percent_change(first_value = 1000, second_value = 42) # -95.8% change
+percent_change(first_value = 100, second_value = 110) # +10% change
