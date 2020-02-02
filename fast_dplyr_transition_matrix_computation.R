f425e2e3f92b4043166a7845b495151e2929ddf0
jacobmunson <43017899+jacobmunson@users.noreply.github.com>
2020-02-01 20:56:39 -0700

    next state from initial state

--- fast_dplyr_transition_matrix_computation.R
+++ fast_dplyr_transition_matrix_computation.R
@@ -1,91 +1,86 @@
-D %>% group_by(user) %>% #filter(user == 36955) %>% 
-  arrange(timestamp) %>% 
-  select(item) %>% 
-  group_by(user) %>% 
-  mutate(next_item = lead(item)) %>% 
-  arrange(user) #%>% filter(user == 36955)
-  
+##############################################################################
+### Building First Order Transition Matrices for Markov Chains using dplyr ###
+##############################################################################
 
-
-item_names = D %>% select(item) %>% .$item
-
-item_transitions 
-
-
-
+## Build a dataset
 num_samples = 1000000
 num_users = 25000
 users = seq(1:num_users)
 states = c("A","B","C","D","E")
 
-
+## States
 da = sample(states, size = num_samples/4, replace = T, prob = c(0.8,0.05,0.05,0.05,0.05))
 db = sample(states, size = num_samples/4, replace = T, prob = c(0.15,0.4,0.15,0.1,0.2))
 dc = sample(states, size = num_samples/4, replace = T, prob = c(0.25,0.15,0.25,0.3,0.25))
 dd = sample(states, size = num_samples/4, replace = T, prob = c(0.35,0.15,0.1,0.3,0.1))
-
 d1 = c(da,db,dc,dd)
 
+## Users
 d2 = sample(users, size = num_samples, replace = T)
 
-Q = data.frame(user = d2, state = d1, stringsAsFactors = F)
-Q = as_tibble(Q)
+## Dataset
+D = data.frame(user = d2, state = d1, stringsAsFactors = F)
+D = as_tibble(D)
 
+## Looking at it
 head(Q)
-Q %>% arrange(user) %>% 
-  group_by(user) %>% 
-  mutate(next_state = lead(state)) %>% 
-  arrange(user)
 
+## Framing up a transition matrix
+# possible transitions
 states_transitions = expand.grid(states,states,stringsAsFactors = F)
 colnames(states_transitions) = c("state","next_state")
+# give each unique transition an identifier
 states_transitions$identifier = seq(1:nrow(states_transitions))
-str(states_transitions)
 
-state_movements = Q %>% arrange(user) %>% 
+## Join on state and next_state to give each transition an identifer
+state_movements = D %>% arrange(user) %>% 
   group_by(user) %>% 
   mutate(next_state = lead(state)) %>% 
   arrange(user) %>% left_join(y = states_transitions) %>% .$identifier
 
-
-actual_movements = merge(data.frame(table(state_movements)), states_transitions, by.x = "state_movements", by.y = "identifier")
-
+## Join frequency of previous step
+actual_movements = merge(data.frame(table(state_movements)), 
+                         states_transitions, 
+                         by.x = "state_movements", by.y = "identifier")
 actual_movements
 
+## Transition Matrix
+transition_matrix = matrix(data = 0, 
+                           nrow = length(states), 
+                           ncol = length(states), 
+                           dimnames = list(states, states))
 
-movement_matrix = matrix(data = 0, 
-                         nrow = length(states), 
-                         ncol = length(states), 
-                         dimnames = list(states, states))
-
+## Fill in Transition Matrix
 for(i in 1:nrow(actual_movements)){
-  
-  movement_matrix[actual_movements[i,"state"], actual_movements[i,"next_state"]] = actual_movements[i,"Freq"]
-    
+  transition_matrix[actual_movements[i,"state"], actual_movements[i,"next_state"]] = actual_movements[i,"Freq"]
 }
 
-movement_matrix
-movement_matrix = movement_matrix/rowSums(movement_matrix)
-round(movement_matrix, 3)
+transition_matrix
+transition_matrix = transition_matrix/rowSums(transition_matrix)
+round(transition_matrix, 3)
 
-movement_matrix = movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix %*% 
-                  movement_matrix %*% movement_matrix
+transition_matrix = transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix %*% 
+                    transition_matrix %*% transition_matrix
 
-round(movement_matrix, 3)
+## Visual Examination
+round(transition_matrix, 3)
 
-movement_matrix * c(0,0,0,1,0)
+
+## Probability of Next Step
+initial_vector = c(0,0,0,1,0)
+transition_matrix * initial_vector
 
 
